1dbdd1320d6ed7e6a179a026a12b71297ead9637
Lelouch Lamperouge <xtechbiz@gmail.com>
2017-12-05 13:36:16 +0800

    updated transition style

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $('#wrapper').fadeIn(500);
+  $('#wrapper').fadeIn(500).css("display", "grid");
   var includes = $('[data-include]');
   jQuery.each(includes, function(){
     var file = 'views/' + $(this).data('include') + '.html';
