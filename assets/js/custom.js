3a77d57864f48b2bc87ddb86feba7990e3cbf438
lordofgeeks <xtechbiz@gmail.com>
2019-02-03 22:03:21 +0800

    wip/redesign for 2019

--- assets/js/custom.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  $('#wrapper').fadeIn(500).css("display", "grid");
-  var includes = $('[data-include]');
-  jQuery.each(includes, function(){
-    var file = 'views/' + $(this).data('include') + '.html';
-    $(this).load(file);
-  });
-});
